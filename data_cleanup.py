9680fc286ca4da69e96b3b9691786403e51df2d7
Youhan Cheery <youhancheery@Youhans-MBP.gateway>
2017-03-08 21:45:00 +1100

    data_cleanup stores all csv files per day into 1 df

--- data_cleanup.py
+++ data_cleanup.py
@@ -26,6 +26,7 @@ def list_files(dir):
                 all_files.append(os.path.join(root, name))                                
             else:
                 # TODO: Need to add a check here for any wrong file types
+                print "Wrong file type detected:", name 
                 continue
 
     return all_files, names
@@ -34,22 +35,18 @@ def list_files(dir):
 # Return a list of all the files within the folder and subfolders
 all_files, names = list_files(DIR)
 
-# ctr = 0
+daily_demand = pd.DataFrame() # initialise total dataframe
 for file in all_files:
-    print file
+    # print file
     data = pd.read_csv(file)
     data.columns = data.iloc[0] # make second row the column row
     # data.reindex(data.index.drop(1)) # drop the second row (original column row)
-    data = data.loc[data['REGIONID'] == 'NSW1']
+    data = data.loc[data['REGIONID'] == 'NSW1'] # keep only NSW data
 
     demand = data.filter(['INTERVAL_DATETIME','OPERATIONAL_DEMAND_POE10',
                           'OPERATIONAL_DEMAND_POE50', 'OPERATIONAL_DEMAND_POE90'], axis=1)
-
-
-    break
-    # if ctr == 2:
-    #     break
-    # ctr += 1
+    tot += len(demand['INTERVAL_DATETIME'])
+    daily_demand = daily_demand.append(demand, ignore_index=True)
 
 if PLOTTING == True:
     plt.plot(demand['OPERATIONAL_DEMAND_POE10'].values, 'r', label='POE10', linewidth=0.75)
