64f8ff04d2c864f7f6f33450ead768ed63aa4017
Youhan Cheery <youhancheery@Youhans-MBP.gateway>
2017-03-12 18:51:59 +1100

    overlayed actual demand on forecasted demand

--- data_cleanup.py
+++ data_cleanup.py
@@ -10,7 +10,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-DIR = '../ForecastedData' # Move back a directory
+FORECASTED_DIR = '../ForecastedData' # Move back a directory
+ACTUAL_DIR = '../ActualData'
 PLOTTING = True
 
 def list_files(dir):
@@ -22,6 +23,7 @@ def list_files(dir):
     for root, _, files in os.walk(dir):
         for name in files:
             if '.CSV' in name:
+                # clean up names to only contain date
                 names.append(name) # probably change to dictionary for when reading in more data
                 all_files.append(os.path.join(root, name))                                
             else:
@@ -33,10 +35,10 @@ def list_files(dir):
 
 
 # Return a list of all the files within the folder and subfolders
-all_files, names = list_files(DIR)
+forecast_files, forecast_names = list_files(FORECASTED_DIR)
 
-daily_demand = pd.DataFrame() # initialise total dataframe
-for file in all_files:
+demand_poe = pd.DataFrame() # initialise total dataframe
+for file in forecast_files:
     print file
     data = pd.read_csv(file)
 
@@ -50,13 +52,45 @@ for file in all_files:
 
     # take only the latest estimate of each interval from the files
     # e.g. for 0030 take 0000 forecast of POE10, POE50, and POE90
-    daily_demand = daily_demand.append(demand.iloc[0], ignore_index=True)
+    demand_poe = demand_poe.append(demand.iloc[0], ignore_index=True)
     # daily_demand = daily_demand.append(demand, ignore_index=True)
-# print daily_demand
+
+
+# TODO: dataframe of multiple days
+
+
+# get actual demand data
+actual_files, actual_names = list_files(ACTUAL_DIR)
+
+actual_demand = pd.DataFrame() # initialise total dataframe
+for file in actual_files:
+    print file
+    data = pd.read_csv(file)
+
+    # slightly hacky solution to remove duplicate OPERATIONAL_DEMAND columns
+    drop_cols = [0,1,2,3]
+    data.drop(data.columns[drop_cols],axis=1,inplace=True)
+
+    data.columns = data.iloc[0] # make second row the column row
+
+    data = data.loc[(data['REGIONID'] == 'NSW1')] # keep only NSW data
+
+    # keep the following columns, discard the remainder
+    actual_demand = data[['INTERVAL_DATETIME','OPERATIONAL_DEMAND']]
+
+
+
 if PLOTTING == True:
-    plt.plot(demand['OPERATIONAL_DEMAND_POE10'].values, 'r', label='POE10', linewidth=0.75)
-    plt.plot(demand['OPERATIONAL_DEMAND_POE50'].values, 'b', label='POE50', linewidth=0.75)
-    plt.plot(demand['OPERATIONAL_DEMAND_POE90'].values, 'g', label='POE90', linewidth=0.75)
+    # plot projected demand @ all POE
+    plt.plot(demand_poe['OPERATIONAL_DEMAND_POE10'].values, 'r', label='POE10', linewidth=0.75)
+    plt.plot(demand_poe['OPERATIONAL_DEMAND_POE50'].values, 'b', label='POE50', linewidth=0.75)
+    plt.plot(demand_poe['OPERATIONAL_DEMAND_POE90'].values, 'g', label='POE90', linewidth=0.75)
+    plt.plot(actual_demand['OPERATIONAL_DEMAND'].values, 'k:', label='Actual Demand', linewidth=1.5)
+
     plt.legend(loc='upper left', shadow=True)
-    plt.title('Demand distributions for %s' % names[0])
+    plt.title('Demand distributions for %s' % forecast_names[0])
+    # plt.show()
+
+
+    # overlay actual demand
     plt.show()
