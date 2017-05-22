did a lot of plotting work. exceedance etc. created testing scrip for error calculation. turns out plots were actually wrong!

error_data.py
@@ -13,59 +13,77 @@ from data_cleanup import get_time_poe
 from plotting import plot_single_day_time_error
 from plotting import plot_single_day_all_time_error
 from plotting import plot_med_avg_error
+from plotting import plot_poex_single_time_error
+from plotting import plot_poex_single_time_full_error
 
 POE10_DIR = '../poe10_error/'
 POE50_DIR = '../poe50_error/'
 POE90_DIR = '../poe90_error/'
 
 # separate the relevant information from data here
-data = OrderedDict()
-filenames = []
-for filename in os.listdir(POE50_DIR):
-    if filename.endswith(".CSV") or filename.endswith(".csv"): 
-        data[filename] = pd.read_csv(POE50_DIR+filename)
-        data[filename] = data[filename].transpose()
-        data[filename].columns = data[filename].iloc[0] # make second row the column row
-        filenames.append(filename) # just in case filenames are needed again
-    else:
-        continue
+def get_data(directory):
+    data = OrderedDict()
+    filenames = []
+    for filename in os.listdir(directory): # change this line for POE level change
+        if filename.endswith(".CSV") or filename.endswith(".csv"): 
+            data[filename] = pd.read_csv(directory+filename) # change this line for POE level change
+            data[filename] = data[filename].transpose()
+            data[filename].columns = data[filename].iloc[0] # make second row the column row
+            filenames.append(filename) # just in case filenames are needed again
+        else:
+            continue
+    return filenames, data
 
-for fname, df in data.iteritems():
+# Need to read everything in this time
+POE10_filenames, POE10_data = get_data(POE10_DIR)
+POE50_filenames, POE50_data = get_data(POE50_DIR)
+POE90_filenames, POE90_data = get_data(POE90_DIR)
+
+
+for fname, df in POE10_data.iteritems():
     print fname
     if fname == "19March2017.csv": # NEED TO REDOWNLOAD DATA FOR THIS DATE
         continue
     '''Plot single day forecast at a single time'''
     date, prefix = convert_date(remove_csv(fname))
     # limit the data to the specified date
-    # print date
-
-    single_day_df = df.filter(regex=date)
+    poe10_single_day = df.filter(regex=date)
+    poe50_single_day = POE50_data[fname].filter(regex=date)
+    poe90_single_day = POE90_data[fname].filter(regex=date)
 
     # limit the data to a specific point in time, e.g. OPERATIONAL_DEMAND_POE10_201703010000 (row 1)
-    data_series = single_day_df.iloc[1,:] # Note that this creates a series
-    time, poe = get_time_poe(data_series.name)
-    print poe
-    # plot_single_day_time_error(data_series, date, time, poe, prefix)
+    data_series = poe10_single_day.iloc[1,:] # Note that this creates a series
+    poe50_series = poe50_single_day.iloc[1,:]
+    poe90_series = poe90_single_day.iloc[1,:]
+    # non-limited data
+    poe10_all = df.iloc[1,:]
+    poe50_all = POE50_data[fname].iloc[1,:]
+    poe90_all = POE90_data[fname].iloc[1,:]
 
+    time, poe = get_time_poe(data_series.name)
+
+    '''To plot single time error for a single POE level'''
+    # plot_single_day_time_error(data_series, date, time, poe, prefix)
+    '''To plot all POE levels at a single time'''
+    # plot_poex_single_time_error(data_series, poe50_series, poe90_series, time, prefix, date)
+    # plot_poex_single_time_full_error(poe10_all, poe50_all, poe90_all, time, prefix, date)
     '''Plot single day forecast at all times'''
     # Get the actual value and plot on top
-    # plot_single_day_all_time_error(single_day_df, date, poe, prefix)
+    plot_single_day_all_time_error(poe10_single_day, date, poe, prefix)
+    plot_single_day_all_time_error(poe50_single_day, date, "POE50", prefix)
+    plot_single_day_all_time_error(poe90_single_day, date, "POE90", prefix)
 
     '''Plot the median and average error on the same plot'''
     '''Should consider doing this for single day forecasts as well'''
-    df = df.transpose()
-    df = df.convert_objects(convert_numeric=True)
-    avg_vals = df.mean(axis=1, skipna=True, numeric_only=True)
-    med_vals = df.median(axis=1, skipna=True, numeric_only=True)
-    
-    # NEED TO PLOT ALL POE10, POE50, and POE90 avg/med errors on the same figure
-    plot_med_avg_error(avg_vals, med_vals, poe, date, prefix)
+    # df = df.transpose()
+    # df = df.convert_objects(convert_numeric=True)
+    # avg_vals = df.mean(axis=1, skipna=True, numeric_only=True)
+    # med_vals = df.median(axis=1, skipna=True, numeric_only=True)
+
+    # plot_med_avg_error(avg_vals, med_vals, poe, date, prefix)
 
     '''Construct a table of median and average data'''
-    avg_vals.to_csv("../avg_error_%s/%s_avg_err.csv" % (poe.lower(), remove_csv(fname)))
-    med_vals.to_csv("../med_error_%s/%s_med_err.csv" % (poe.lower(), remove_csv(fname)))
+    # avg_vals.to_csv("../avg_error_%s/%s_avg_err.csv" % (poe.lower(), remove_csv(fname)))
+    # med_vals.to_csv("../med_error_%s/%s_med_err.csv" % (poe.lower(), remove_csv(fname)))
 
-    '''Construct an average scalar of each error. e.g. for 01/03/2017 average POE10 error = x,
-    average POE50 error = y, average POE90 error = z. Use this to construct a histogram.'''
-    # print avg_vals
     # exit()

operate_avg.py
@@ -4,12 +4,13 @@ average POE50 error = y, average POE90 error = z. Use this to construct a histog
 
 # need to do:
 # 1. all average POE errors on the same plot
-# 2. when not on same figure, include horizontal line of overall average error
+# 2. count number of times error exceeds 0
 # 3. create csv file containing the single-value average of each day (should be easy after doing 2)
 # 4. create a histogram of these daily average error values for each POE level
 
 
 import os
+import numpy as np
 import pandas as pd
 from collections import OrderedDict
 import plotting
@@ -37,6 +38,9 @@ def get_data(directory):
     return filenames, data
 
 
+def exceeds_zero_error(df):
+    return df.values[1][np.where(df.values[1] > 0)].size
+
 # Need to read everything in this time
 POE10_filenames, POE10_data = get_data(POE10_DIR)
 POE50_filenames, POE50_data = get_data(POE50_DIR)
@@ -47,14 +51,27 @@ POE90_filenames, POE90_data = get_data(POE90_DIR)
 for fname, df in POE10_data.iteritems(): # dfs contain the average error values of the POE level specified above
     if fname == "19March2017.csv": # This date is broken.
         continue
-
+    print fname
     # get the date and prefix of date (0, 1, 2, etc)
     date, prefix = convert_date(remove_csv(fname))
 
+    # Plot all POE errors together
+    # plotting.plot_all_errors_one_graph(fname, df, POE50_data[fname], POE90_data[fname], date, prefix)
 
-    plotting.plot_all_errors_one_graph(fname, df, POE50_data[fname], POE90_data[fname], date, prefix)
+    # Count the number of times POEx exceeds 0 error
+    tot = df.values[1].size
+    poe10_exceedance = exceeds_zero_error(df)
+    poe50_exceedence = exceeds_zero_error(POE50_data[fname])
+    poe90_exceedence = exceeds_zero_error(POE90_data[fname])
+    x_ax = [poe10_exceedance, poe50_exceedence, poe90_exceedence]
+    x_prob = [1 - poe10_exceedance/float(tot), 1 - poe50_exceedence/float(tot), 1 - poe90_exceedence/float(tot)]
+    # Plot histogram
+    plotting.plot_exceedance_num(x_ax, prefix, date)
 
+    # Get average error value
 
-    exit()
+    # Plot histogram of average error
+
+    # exit()
 
 